469db68d4f709024c6e71748212b8c5fe947bf8c
lauraamelina <lauraamelina@gmail.com>
2024-06-05 12:31:22 -0300

    use dotenv to enviroment variables

--- services/connection.js
+++ services/connection.js
@@ -1,12 +1,14 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const connectToDatabase = async () => {
     try {
         const connection = await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'root',
-            database: 'tasks'
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
         });
 
         console.log('Conexión a la base de datos establecida');
