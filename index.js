f992a54def9cdf7fd00ffb326767799cda919698
lauraamelina <lauraamelina@gmail.com>
2024-06-05 15:06:51 -0300

    use and install cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors'
 import UserRoute from './routes/user.api.route.js'
 import TaskRoute from './routes/task.api.route.js'
 import StatusRoute from './routes/status.api.route.js'
@@ -7,14 +8,11 @@ import StatusRoute from './routes/status.api.route.js'
 const app = express();
 app.use(bodyParser.json());
 app.set('port', 8080);
+app.use(cors())
 
 app.use('/users/', UserRoute)
 app.use('/task/', TaskRoute)
 app.use('/status/', StatusRoute)
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 app.listen(app.get('port'), () => {
     console.log('Servidor corriendo en el puerto', app.get('port'));
